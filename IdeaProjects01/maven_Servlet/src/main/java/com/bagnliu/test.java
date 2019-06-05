9b5d12f378bc48dff88c9968e26e874f782d5d50
bagnliu <bagnliu@163.com>
2019-06-05 20:03:09 +0800

    通过IDEA搭建maven方式的第一个Servlet项目

---
+++ IdeaProjects01/maven_Servlet/src/main/java/com/bagnliu/test.java
@@ -0,0 +1,11 @@
+package com.bagnliu;
+
+/**
+ * @author bagnliu
+ * @create 2019-06-05 20:02
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("...............");
+    }
+}
